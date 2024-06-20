001e79f1b0d6d8ce78c71b396ab2b8eac93ec2eb
Eslam Amin <Eslam.amin.610@gmail.com>
2024-06-20 16:18:43 +0300

    Version 3.1.2
    Fixes send mail issues

--- utils/email.js
+++ utils/email.js
@@ -1,18 +1,40 @@
 
 const nodeMailer = require("nodemailer");
 
+const sendEmailGoogle = async options => {
 
-const sendEmail1 = async options => {
+    const transporter = nodeMailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT, // secure = false => 587
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+    // Email Options
+    const mailOptions = {
+        // from: `${process.env.APP_NAME} <MAIL>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        html: options.html
+    };
+    // Send email
+    await transporter.sendMail(mailOptions);
+}
+
+//Outlook
+const sendEmail = async options => {
     let transporter = nodeMailer.createTransport({
-
         host: "smtp-mail.outlook.com",
         auth: {
             user: process.env.OUTLOOK_EMAIL,
-            pass: process.env.OUTLOOK.PASSOWRD
+            pass: process.env.OUTLOOK_PASSWORD
         }
     })
     const mailOptions = {
-        // from: "Eslam Amin <admin@social-network.com>",
+        from: "Eslam Amin <ea.eslamamin@outlook.com>",
         to: options.email,
         subject: options.subject,
         text: options.message,
@@ -23,7 +45,7 @@ const sendEmail1 = async options => {
 }
 
 
-const sendEmail = async options => {
+const sendEmail1 = async options => {
     //Create a transporter
     const transporter = nodeMailer.createTransport({
         host: "sandbox.smtp.mailtrap.io",
@@ -32,21 +54,8 @@ const sendEmail = async options => {
             user: process.env.MAILTRAP_EMAIL,
             pass: process.env.MAILTRAP_PASSWORD
         }
-        // host: process.env.EMAIL_HOST,
-        // port: process.env.EMAIL_PORT,
-        // auth: {
-        //     user: process.env.EMAIL,
-        //     pass: process.env.PASSWORD
-        // },
-        // Activate in gmail "less secure app" option
-        // service: "Gmail",
-        // auth: {
-        //     user: process.env.EMAIL,
-        //     pass: process.env.PASSWORD
-        // },
-        //Activate in gmail "less secure app" option
-    })
 
+    })
     //define email options
     const mailOptions = {
         from: "Eslam Amin <admin@social-network.com>",
