7d1451cce2e90fa277170d71fb96bfe90eb31f87
Eslam Amin <Eslam.amin.610@gmail.com>
2024-06-20 20:23:47 +0300

    Trying to solve email configuration

--- utils/email.js
+++ utils/email.js
@@ -28,9 +28,15 @@ const sendEmailGoogle = async options => {
 const sendEmail = async options => {
     let transporter = nodeMailer.createTransport({
         host: "smtp-mail.outlook.com",
+        port: 587,  // Port for TLS
+        secure: false, // true for 465, false for other ports
+
         auth: {
             user: process.env.OUTLOOK_EMAIL,
             pass: process.env.OUTLOOK_PASSWORD
+        },
+        tls: {
+            ciphers: 'SSLv3'
         }
     })
     const mailOptions = {
